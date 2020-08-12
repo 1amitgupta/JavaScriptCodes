9f21f31e9cfff00be32d8f2032201d199f8e3ac7
Amit Gupta <42915812+1amitgupta@users.noreply.github.com>
2020-08-12 16:14:15 +0530

    Create input-select-filed.js

---
+++ input-select-filed.js
@@ -0,0 +1,17 @@
+/**
+ * This code set previous value for select input fields,
+ * when confirm prompt return false.
+ */
+var val;
+$('.f1m-table').on('focus', '.helpstatusform', function() {
+  val = $(this).val();
+  console.log(val);
+}).on('change', '.helpstatusform', function() {
+  if (confirm('Are you sure ?')) {
+    $(this).parent('form').submit();
+    return true;
+  }
+
+  $(this).val(val);
+  return false;
+});
